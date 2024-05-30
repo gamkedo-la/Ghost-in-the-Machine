9d06515413f035c128517c396e786da1995ca783
Christer Kaitila <breakdance@mcfunkypants.com>
2024-05-30 13:59:43 -0700

    update sky image, sky now scrolls+loops

--- js/Background.js
+++ js/Background.js
@@ -1,6 +1,14 @@
 // Background.js
 // drawns the sky and floor
 
+const WIND_SPEED = -0.02; // (pixels per ms)
+
+// FIXME: guessed this magic number: use math instead!
+// so that the sky image offset exactly matches player.rot  
+// I guess we need to calculate...
+// how many radians per canvas pixel (using FOV + canvas width)
+const SKY_ROTATION_FACTOR = -320;
+
 var skyTexture = new Image();
 skyTexture.src = './images/sky.png';
 skyTexture.onload = function() { this.loaded=true; }
@@ -14,15 +22,12 @@ function drawBackground() {
     if (!skyTexture.loaded) return;
     if (!floorTexture.loaded) return;
 
-    // FIXME: guessed this magic number: use math instead!
-    // I guess we need to calculate
-    // how many radians per canvas pixel (using FOV + canvas width)
-    
     // scrolling tiled sky image based on player rotation
-    let skyX = Math.round(player.rot * -320) % skyTexture.width;
+    let wind_offset = performance.now() * WIND_SPEED;
+    let skyX = Math.round(wind_offset + (player.rot * SKY_ROTATION_FACTOR)) % skyTexture.width;
     let skyY = Math.round(canvas.height/2-skyTexture.height);
-    console.log("sky rot:"+player.rot.toFixed(2)+" pos:"+skyX);
-  	canvasContext.drawImage(skyTexture,skyX,skyY);
+    //console.log("sky rot:"+player.rot.toFixed(2)+" pos:"+skyX);
+    canvasContext.drawImage(skyTexture,skyX,skyY);
   	canvasContext.drawImage(skyTexture,skyX+skyTexture.width,skyY);
 
   	// a single static floor gradient with no perspective or rotation
