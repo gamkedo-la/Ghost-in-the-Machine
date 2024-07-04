942f70f0f4af2c6c8da1e86ac2face932f92d8ef
jetimms <6677676+jetimms@users.noreply.github.com>
2024-07-04 12:33:14 -0400

    AI Pathfinding: Heap improvements to accept complex comparisons of { position, cost } from the priority queue.

--- js/Heap.js
+++ js/Heap.js
@@ -1,59 +1,82 @@
 const HEAP_LOOP_MAX = 1000;
 
+class MinHeap extends Heap {
+    constructor(compareFxIn) {
+      super(false, compareFxIn)
+    }
+}
+
+class MaxHeap extends Heap {
+    constructor(compareFxIn) {
+      super(true, compareFxIn)
+    }
+}
+
 class Heap {
-    constructor(isMaxHeap = false) {
-        this.data = [];
-        this.isMaxHeap = isMaxHeap;
+    #data = [];
+    #basicDiff = (a, b) => a - b; 
+    #compareFx = this.#basicDiff;
+    #isMaxHeap = false;
+
+    constructor(isMaxHeapIn, compareFxIn) {
+        this.#isMaxHeap ??= isMaxHeapIn;
+        this.#compareFx ??= compareFxIn;
     }
 
-    minCompare(a, b) {
-        return a < b ? 1 : (a > b ? -1 : 0);
+    constructor() {
+        this.constructor(false);
     }
 
-    maxCompare(a, b) {
-        return -1 * this.minCompare(a, b);
-    }
-
-    compare(a, b) {
-        let compareAlgo = this.isMaxHeap ? this.maxCompare : this.minCompare;
-        return compareAlgo(a, b);
-    }
+    get isMaxHeap() { return this.#isMaxHeap; }
 
     add(value) {
-        this.data.push(value); 
-        this.heapifyUp();
+        this.#data.push(value); 
+        this.#heapifyUp();
     }
 
     poll() {
         // get root value
-        let returnValue = this.data[0];
-        let lastElement = this.data.pop();
-        if (this.data.length > 0) {
+        let returnValue = this.#data[0];
+        let lastElement = this.#data.pop();
+        if (this.#data.length > 0) {
             // move last element to root
-            this.data[0] = lastElement;
-            this.heapifyDown();
+            this.#data[0] = lastElement;
+            this.#heapifyDown();
         }
         return returnValue;
     }
 
-    heapifyUp() {
-        let childIndex = this.data.length - 1;
+    #minCompare(a, b) {
+        return this.#compareFx(a, b) < 0;
+    }
+
+    #maxCompare(a, b) {
+        return this.#compareFx(a, b) > 0;
+    }
+
+    #compare(a, b) {
+        let compareAlgo = this.#isMaxHeap ? this.#maxCompare : this.#minCompare;
+        return compareAlgo(a, b);
+    }
+
+    #heapifyUp() {
+        let childIndex = this.#data.length - 1;
         if (childIndex > 0) {
-            const childValue = () => this.data[childIndex];
+            const childValue = () => this.#data[childIndex];
             const parentIndex = () => Math.max(0, Math.floor((childIndex - 1) / 2));
-            const parentValue = () => this.data[parentIndex()];
+            const parentValue = () => this.#data[parentIndex()];
             let i = 0;
             function maxLoopsReached() { i++ >= HEAP_LOOP_MAX; }
 
             const keepLooping = () => 
                 childIndex > 0 &&
-                this.compare(childValue(), parentValue()) === 1 && 
+                this.#compare(childValue(), parentValue()) && 
                 !maxLoopsReached();
 
             const swapValues = (iP, iC, vP, vC) => {
                 const temp = vP;
-                this.data[iP] = vC;
-                this.data[iC] = temp;
+                this.#data[iP] = vC;
+                this.#data[iC] = temp;
             }
 
             while (keepLooping() === true) {
@@ -67,39 +90,46 @@ class Heap {
         }
     }
 
-    heapifyDown() {
-        if (this.data.length > 1) {
+    #heapifyDown() {
+        if (this.#data.length > 1) {
             let parentIndex = 0;
-            const parentValue = () => this.data[parentIndex];
-            const childIndex2 = () => Math.floor((parentIndex + 1) * 2);
-            const childIndex1 = () => childIndex2() - 1;
-            const childValue1 = () => this.data[childIndex1()];
-            const childValue2 = () => this.data[childIndex2()];
             let i = 0;
-            function maxLoopsReached() { i++ >= HEAP_LOOP_MAX; }
+            const maxLoopsReached = () => i++ >= HEAP_LOOP_MAX;
 
-            let noSwaps = false;
-            const keepLooping = () => 
-                childIndex1() < this.data.length &&
-                !noSwaps &&
-                !maxLoopsReached();
+            let noSwapsYet = false;
+            const keepLooping = () => !noSwapsYet && !maxLoopsReached();
 
             const swapValues = (iP, iC, vP, vC) => {
                 const temp = vP;
-                this.data[iP] = vC;
-                this.data[iC] = temp;
+                this.#data[iP] = vC;
+                this.#data[iC] = temp;
             }
 
+            const indexInBounds = (index = this.#data.length) => 
+                index < this.#data.length;
+
+            const getDataValue = (index) => 
+                indexInBounds(index) ? this.#data[index] : null;
+
             while (keepLooping()) {
-                if (this.compare(childValue1(), parentValue()) === 1) {
-                    swapValues(parentIndex, childIndex1(), parentValue(), childValue1());
-                    parentIndex = childIndex1();
-                } else if (
-                    childIndex2() < this.data.length &&
-                    this.compare(childValue2(), parentValue()) === 1
+                let parentValue = this.#data[parentIndex];
+                let childIndex2 = Math.floor((parentIndex + 1) * 2);
+                let childIndex1 = childIndex2 - 1;
+                let childValue1 = getDataValue(childIndex1);
+                let childValue2 = getDataValue(childIndex2);
+
+                if (
+                    childIndex1 < this.#data.length &&
+                    this.#compare(childValue1, parentValue)
                 ) {
-                    swapValues(parentIndex, childIndex2(), parentValue(), childValue2());
-                    parentIndex = childIndex2();
+                    swapValues(parentIndex, childIndex1, parentValue, childValue1);
+                    parentIndex = childIndex1;
+                } else if (
+                    childIndex2 < this.#data.length &&
+                    this.#compare(childValue2, parentValue)
+                ) {
+                    swapValues(parentIndex, childIndex2, parentValue, childValue2);
+                    parentIndex = childIndex2;
                 } else {
                     noSwaps = true;
                 }
